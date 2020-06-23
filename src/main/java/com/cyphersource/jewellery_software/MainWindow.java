27c58abe98596e6183ff0585217d0f0ca4812b45
Logidasakthi <banulogida2000@gmail.com>
2020-06-23 15:22:22 +0530

    [IMP]viewPage : The overall page is completely finished with the back-end work with no possible errors
    The overall page is corrected of small bugs leftover. It is successfully completed.

--- src/main/java/com/cyphersource/jewellery_software/MainWindow.java
+++ src/main/java/com/cyphersource/jewellery_software/MainWindow.java
@@ -37,7 +37,7 @@ public class MainWindow extends javax.swing.JFrame {
     PreparedStatement pat = null;
     Statement stmt=null;
     ResultSet rs = null;
-    String view_ornament_type_data;
+    String view_ornament_type1_data;
     String view_from1_dateformat;
     String view_to1_dateformat;
     String view_from1_date;
@@ -1039,67 +1039,83 @@ public class MainWindow extends javax.swing.JFrame {
     private void view_selOrnament1_dropdownview_selOrnament_dropdownActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_view_selOrnament1_dropdownview_selOrnament_dropdownActionPerformed
        // TODO add your handling code here:
            
-         
-            try{ 
-                try{
-                    view_ornament_type_data=view_selOrnament1_dropdown.getSelectedItem().toString();
-                    
-                    //Getting table values acc. to selOrnament
-                    String sql="SELECT id, date, chase_no, ornament_type, ornament_name, quality,\n" +
-                    "making_charge, weight, wastage, quantity,buy, barcode, status FROM overall\n" +
-                    "WHERE ornament_type =" + "'"+  view_ornament_type_data + "'";
-                    con = DriverManager.getConnection("jdbc:mysql://localhost:3306/JAJ","root","");
-                    pat=con.prepareStatement(sql);
-                    rs=pat.executeQuery();
-                    DefaultTableModel tm=(DefaultTableModel)view_table1_table.getModel();
-                    tm.setRowCount(0);
-                    while(rs.next()){
-                        Object o[]={rs.getInt("id"),rs.getString("date"),rs.getString("chase_no"),rs.getString("ornament_name"),rs.getString("weight"),rs.getString("wastage"),rs.getString("making_charge"),rs.getString("quantity"),rs.getString("quality"),rs.getString("buy")};
-                        tm.addRow(o);
+           view_ornament_type1_data=view_selOrnament1_dropdown.getSelectedItem().toString();
+           
+           if(view_ornament_type1_data!="Select the Ornament"){
+ 
+                try{ 
+                    try{
+
+
+                        //Getting table values acc. to selOrnament
+                        String sql="SELECT id, date, chase_no, ornament_type, ornament_name, quality,\n" +
+                        "making_charge, weight, wastage, quantity,buy, barcode, status FROM overall\n" +
+                        "WHERE ornament_type =" + "'"+  view_ornament_type1_data + "'";
+                        con = DriverManager.getConnection("jdbc:mysql://localhost:3306/JAJ","root","");
+                        pat=con.prepareStatement(sql);
+                        rs=pat.executeQuery();
+                        DefaultTableModel tm=(DefaultTableModel)view_table1_table.getModel();
+                        tm.setRowCount(0);
+                        while(rs.next()){
+                            Object o[]={rs.getInt("id"),rs.getString("date"),rs.getString("chase_no"),rs.getString("ornament_name"),rs.getString("weight"),rs.getString("wastage"),rs.getString("making_charge"),rs.getString("quantity"),rs.getString("quality"),rs.getString("buy")};
+                            tm.addRow(o);
+                        }
+
+                    } 
+                    catch(Exception e){
+                        JOptionPane.showMessageDialog(null,e);
                     }
-                    
-                } 
-                catch(Exception e){
-                    JOptionPane.showMessageDialog(null,e);
+
+                    try{
+
+                       //Getting total weight of items acc. to selOrnament.
+                       String sql1="SELECT SUM(weight) FROM overall WHERE ornament_type = " + "'"+  view_ornament_type1_data + "'";
+                       con1 = DriverManager.getConnection("jdbc:mysql://localhost:3306/JAJ","root","");
+                       PreparedStatement pat1=con1.prepareStatement(sql1);
+                       ResultSet rs1=pat1.executeQuery();
+                       while(rs1.next()){
+                            if(rs1.getString(1)==null){
+                                view_totWtInp1_label.setText("0"); 
+                            }
+                            else{
+                                view_totWtInp1_label.setText(rs1.getString(1)); 
+                            }
+                       } 
+                      
+                    } 
+                    catch(Exception e){
+                        JOptionPane.showMessageDialog(null,e);
+                    } 
+
+                    try{
+
+                       //Getting total number of items acc. to selOrnament.
+                       String sql2="SELECT COUNT(id) FROM overall WHERE ornament_type = " + "'"+  view_ornament_type1_data + "'";
+                       con2 = DriverManager.getConnection("jdbc:mysql://localhost:3306/JAJ","root","");
+                       PreparedStatement pat2=con2.prepareStatement(sql2);
+                       ResultSet rs2=pat2.executeQuery();
+                       while(rs2.next()){
+                           view_totItemInp1_label.setText(rs2.getString(1)); 
+                       }
+                    } 
+                    catch(Exception e){
+                        JOptionPane.showMessageDialog(null,e);
+                    }
+                    con.close();
+                    con1.close();
+                    con2.close();
                 }
-                
-                try{
-                    
-                   //Getting total weight of items acc. to selOrnament.
-                   String sql1="SELECT SUM(weight) FROM overall WHERE ornament_type = " + "'"+  view_ornament_type_data + "'";
-                   con1 = DriverManager.getConnection("jdbc:mysql://localhost:3306/JAJ","root","");
-                   PreparedStatement pat1=con1.prepareStatement(sql1);
-                   ResultSet rs1=pat1.executeQuery();
-                   while(rs1.next()){
-                       view_totWtInp1_label.setText(rs1.getString(1)); 
-                   }  
-                } 
                 catch(Exception e){
-                    JOptionPane.showMessageDialog(null,e);
-                } 
-                
-                try{
-                    
-                   //Getting total number of items acc. to selOrnament.
-                   String sql2="SELECT COUNT(id) FROM overall WHERE ornament_type = " + "'"+  view_ornament_type_data + "'";
-                   con2 = DriverManager.getConnection("jdbc:mysql://localhost:3306/JAJ","root","");
-                   PreparedStatement pat2=con2.prepareStatement(sql2);
-                   ResultSet rs2=pat2.executeQuery();
-                   while(rs2.next()){
-                       view_totItemInp1_label.setText(rs2.getString(1)); 
-                   }
-                } 
-                catch(Exception e){
-                    JOptionPane.showMessageDialog(null,e);
+                        JOptionPane.showMessageDialog(null,e);
                 }
-                con.close();
-                con1.close();
-                con2.close();
+                view_combined_display();
             }
-            catch(Exception e){
-                    JOptionPane.showMessageDialog(null,e);
+            else if((view_from1_date!=null)&&(view_to1_date!=null)){
+                        view_date_display();
             }
-            view_combined_display();
+            else{
+                        view_default_display();
+            }
     }//GEN-LAST:event_view_selOrnament1_dropdownview_selOrnament_dropdownActionPerformed
 
     private void view_selOrnament4_dropdownview_selOrnament_dropdownActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_view_selOrnament4_dropdownview_selOrnament_dropdownActionPerformed
@@ -1323,15 +1339,15 @@ public class MainWindow extends javax.swing.JFrame {
         }   
     }
     
-  
-    private void view_dropdown_display(){
+    // Displaying dropdown items from database
+    private void view_dropdown_display(){  
         try{
             String sql="SELECT type FROM Ornament_type";
             con = DriverManager.getConnection("jdbc:mysql://localhost:3306/JAJ","root","");
             stmt=con.createStatement();
             rs=stmt.executeQuery(sql);
             while(rs.next()){
-             view_selOrnament1_dropdown.addItem(rs.getString("type"));
+                view_selOrnament1_dropdown.addItem(rs.getString("type"));
             }
         }
         catch(Exception e){
@@ -1348,8 +1364,14 @@ public class MainWindow extends javax.swing.JFrame {
                     PreparedStatement pat1=con.prepareStatement(sql1);
                     ResultSet rs1=pat1.executeQuery();
                     while(rs1.next()){
-                        view_totWtInp1_label.setText(rs1.getString(1)); 
+                         if(rs1.getString(1)==null){
+                            view_totWtInp1_label.setText("0"); 
+                         }
+                         else{
+                            view_totWtInp1_label.setText(rs1.getString(1)); 
+                         }
                     }
+                    
                 }
                 catch(Exception e){
                     JOptionPane.showMessageDialog(null,e);
@@ -1424,7 +1446,12 @@ public class MainWindow extends javax.swing.JFrame {
                         PreparedStatement pat1=con1.prepareStatement(sql1);
                         ResultSet rs1=pat1.executeQuery();
                         while(rs1.next()){
-                            view_totWtInp1_label.setText(rs1.getString(1)); 
+                             if(rs1.getString(1)==null){
+                                    view_totWtInp1_label.setText("0"); 
+                             }
+                             else{
+                                    view_totWtInp1_label.setText(rs1.getString(1));
+                             }
                         }  
                     }
                     catch(Exception e){
@@ -1450,56 +1477,65 @@ public class MainWindow extends javax.swing.JFrame {
     }
     
     private void view_combined_display(){
-        if((view_ornament_type_data!=null) &&(view_from1_date!=null) && (view_to1_date!=null)){
-                try{ 
-                        
-                        //Displaying table according to selOrnament and dates
-                        String sql1="SELECT * FROM overall WHERE ornament_type = " + "'"+  view_ornament_type_data + "' AND date >=" + "'"+  view_from1_date + "'  AND date <= " + "'"+  view_to1_date + "'";
-                        con = DriverManager.getConnection("jdbc:mysql://localhost:3306/JAJ","root","");
-                        PreparedStatement pat1=con.prepareStatement(sql1);
-                        ResultSet rs1=pat1.executeQuery();
-                        DefaultTableModel tm=(DefaultTableModel)view_table1_table.getModel();
-                        tm.setRowCount(0);
-                        while(rs1.next()){
-                            Object o[]={rs1.getInt("id"),rs1.getString("date"),rs1.getString("chase_no"),rs1.getString("ornament_name"),rs1.getString("weight"),rs1.getString("wastage"),rs1.getString("making_charge"),rs1.getString("quantity"),rs1.getString("quality"),rs1.getString("buy")};
-                            tm.addRow(o);
-                        } 
-                }
-                catch(Exception e){
-                        JOptionPane.showMessageDialog(null,e);
-                }
+        
+            if((view_ornament_type1_data!=null) &&(view_from1_date!=null) && (view_to1_date!=null)){
                 
-                try{
-                        
-                        //Getting total weight of items acc. to selOrnament and dates.
-                        String sql1="SELECT SUM(weight) FROM overall WHERE ornament_type = " + "'"+  view_ornament_type_data + "' AND date >=" + "'"+  view_from1_date + "'  AND date <= " + "'"+  view_to1_date + "'";
-                        con1 = DriverManager.getConnection("jdbc:mysql://localhost:3306/JAJ","root","");
-                        PreparedStatement pat1=con1.prepareStatement(sql1);
-                        ResultSet rs1=pat1.executeQuery();
-                        while(rs1.next()){
-                            view_totWtInp1_label.setText(rs1.getString(1)); 
-                        }  
+                if(view_ornament_type1_data!="Select the Ornament"){
+                    try{ 
+
+                            //Displaying table according to selOrnament and dates
+                            String sql1="SELECT * FROM overall WHERE ornament_type = " + "'"+  view_ornament_type1_data + "' AND date >=" + "'"+  view_from1_date + "'  AND date <= " + "'"+  view_to1_date + "'";
+                            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/JAJ","root","");
+                            PreparedStatement pat1=con.prepareStatement(sql1);
+                            ResultSet rs1=pat1.executeQuery();
+                            DefaultTableModel tm=(DefaultTableModel)view_table1_table.getModel();
+                            tm.setRowCount(0);
+                            while(rs1.next()){
+                                Object o[]={rs1.getInt("id"),rs1.getString("date"),rs1.getString("chase_no"),rs1.getString("ornament_name"),rs1.getString("weight"),rs1.getString("wastage"),rs1.getString("making_charge"),rs1.getString("quantity"),rs1.getString("quality"),rs1.getString("buy")};
+                                tm.addRow(o);
+                            } 
+                    }
+                    catch(Exception e){
+                            JOptionPane.showMessageDialog(null,e);
+                    }
+
+                    try{
+
+                            //Getting total weight of items acc. to selOrnament and dates.
+                            String sql1="SELECT SUM(weight) FROM overall WHERE ornament_type = " + "'"+  view_ornament_type1_data + "' AND date >=" + "'"+  view_from1_date + "'  AND date <= " + "'"+  view_to1_date + "'";
+                            con1 = DriverManager.getConnection("jdbc:mysql://localhost:3306/JAJ","root","");
+                            PreparedStatement pat1=con1.prepareStatement(sql1);
+                            ResultSet rs1=pat1.executeQuery();
+                            while(rs1.next()){
+                                 if(rs1.getString(1)==null){
+                                    view_totWtInp1_label.setText("0"); 
+                                 }
+                                 else{
+                                    view_totWtInp1_label.setText(rs1.getString(1));
+                                 }
+                            } 
+                    }
+                    catch(Exception e){
+                             JOptionPane.showMessageDialog(null,e);
+                    }
+
+                    try{
+
+                            //Getting total number of items acc. to selOrnament and dates.
+                            String sql2="SELECT COUNT(id) FROM overall WHERE ornament_type = " + "'"+  view_ornament_type1_data + "' AND date >=" + "'"+  view_from1_date + "'  AND date <= " + "'"+  view_to1_date + "'";
+                            con2 = DriverManager.getConnection("jdbc:mysql://localhost:3306/JAJ","root","");
+                            PreparedStatement pat2=con2.prepareStatement(sql2);
+                            ResultSet rs2=pat2.executeQuery();
+                            while(rs2.next()){
+                                view_totItemInp1_label.setText(rs2.getString(1)); 
+                            }
+                    } 
+                    catch(Exception e){
+                            JOptionPane.showMessageDialog(null,e);
+                    }
                 }
-                catch(Exception e){
-                         JOptionPane.showMessageDialog(null,e);
-                }
-                
-                try{
-                        
-                        //Getting total number of items acc. to selOrnament and dates.
-                        String sql2="SELECT COUNT(id) FROM overall WHERE ornament_type = " + "'"+  view_ornament_type_data + "' AND date >=" + "'"+  view_from1_date + "'  AND date <= " + "'"+  view_to1_date + "'";
-                        con2 = DriverManager.getConnection("jdbc:mysql://localhost:3306/JAJ","root","");
-                        PreparedStatement pat2=con2.prepareStatement(sql2);
-                        ResultSet rs2=pat2.executeQuery();
-                        while(rs2.next()){
-                            view_totItemInp1_label.setText(rs2.getString(1)); 
-                        }
-                } 
-                catch(Exception e){
-                        JOptionPane.showMessageDialog(null,e);
-                }
-                 
-        }
-    }    
+            }
+    } 
+    
 }
 
